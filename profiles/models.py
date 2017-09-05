920be0836979589194ce90d157377db426435c6f
amosISA <amosisa700@gmail.com>
2017-09-05 08:35:07 +0200

    Added followers to profile user

--- profiles/models.py
+++ profiles/models.py
@@ -2,5 +2,25 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models.signals import post_save
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    followers = models.ManyToManyField(User, related_name='is_following', blank=True)
+    following = models.ManyToManyField(User, related_name='following', blank=True)
+    activated = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.user.username
+
+def post_save_user_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        profile, is_created = Profile.objects.get_or_create(user=instance)
+
+post_save.connect(post_save_user_receiver, sender=User)
