af1739647af0c162a557c048c7ff41fb39e2a0db
amosISA <amosisa700@gmail.com>
2017-09-14 11:33:13 +0200

    Added new styles to create post (img)

---
+++ posts/static/posts/js/file_image.js
@@ -0,0 +1,30 @@
+$(function() {
+    
+    // Clear event
+    $('.image-preview-clear').click(function(){
+        $('.image-preview-filename').val("");
+        $('.image-preview-clear').hide();
+        $('.image-preview-input input:file').val("");
+        $(".image-preview-input-title").text("Browse");
+    });
+
+    // Create the preview image
+    $(".image-preview-input input:file").change(function (){
+        var img = $('<img/>', {
+            id: 'dynamic',
+            width:250,
+            height:200
+        });
+        var file = this.files[0];
+        var reader = new FileReader();
+        // Set preview image into the popover data-content
+        reader.onload = function (e) {
+            $(".image-preview-input-title").text("Change");
+            $(".image-preview-clear").show();
+            $(".image-preview-filename").val(file.name);
+            img.attr('src', e.target.result);
+        }
+        reader.readAsDataURL(file);
+    });
+
+});
