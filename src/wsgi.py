e978288f95b4eb5b325a1097319a51cb1be3e62f
amosISA <amosisa700@gmail.com>
2017-10-04 15:50:06 +0200

    Modified wsgi

--- src/wsgi.py
+++ src/wsgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sharing_things.src.settings")
+# path a donde esta el manage.py de nuestro proyecto Django
+sys.path.append('/home/admin/sharing_things/sharing_things/')
+
+# referencia (en python) desde el path anterior al fichero settings.py
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
+
+# prevenimos UnicodeEncodeError
+os.environ.setdefault('LANG', 'en_US.UTF-8')
+os.environ.setdefault('LC_ALL', 'en_US.UTF-8')
 
 application = get_wsgi_application()
